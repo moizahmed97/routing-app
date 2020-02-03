a5fb6dc62534eae739d3970a1324f5d7a3050627
moizahmed97 <moizahmed97@gmail.com>
2020-02-03 17:19:15 +0300

    delete dispplay.html to shift to single page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,url_for, render_template, request,redirect,session,jsonify
 from connect import client
 from sampledata import firstCoordinate,secondCoordinate
+from bson.json_util import dumps
+import json
+
 
 db = client.routes # Database is called routes
 
@@ -10,29 +13,13 @@ coordinates = db.coordinates # collection (table) called coordinates
 # coordinates.insert_many([firstCoordinate, secondCoordinate]) # Insert document (row) into collection (table) people
 
 app = Flask(__name__)
-# required for session management
-app.secret_key = "skdlfj235647fhf"
-
-sampleList = []
-
-sampleList.append(firstCoordinate)
-sampleList.append(secondCoordinate)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        allCoordinates = coordinates.find()  # get all the coordinates as objects from the Database
+        allCoordinates = dumps(coordinates.find())  # get all the coordinates as objects from the Database
+        allCoordinates = json.loads(allCoordinates)
         return render_template("index.html", data = allCoordinates)
-    elif request.method == 'POST':
-        # get the data sent by AJAX from index.html
-        test = request.form["check"]
-        print(test)
-        return redirect(url_for('display'))
-
-
-@app.route("/display")
-def display():
-    return render_template("display.html")
 
 
 @app.route("/about")
@@ -43,11 +30,3 @@ def about():
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
-"""  @app.after_request
-def add_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    return response
-"""
